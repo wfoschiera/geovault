b80a9908495ab0ef88c97abfcacc7aaa7788174a
William Foschiera <wfoschiera@gmail.com>
2025-04-12 14:58:58 -0400

    remove hardcoded secret_key.

--- geovault/settings.py
+++ geovault/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v!tlfojmd-@8_s4d&*mexuc1nk0ag5k=7j3a=x_4_@&&v^(+u0"
+SECRET_KEY = config("django_secret_key", default="my_hidden_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
