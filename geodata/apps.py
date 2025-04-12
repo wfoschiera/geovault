65d2e19640280432abf6362b47a79c403fc2f1b9
William Foschiera <wfoschiera@gmail.com>
2025-04-12 15:15:30 -0400

    linter

--- geodata/apps.py
+++ geodata/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GeodataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geodata'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "geodata"
