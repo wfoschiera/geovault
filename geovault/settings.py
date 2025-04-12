044bc24c7eaa7a078c65539c294fd2a48c71e35a
William Foschiera <wfoschiera@gmail.com>
2025-04-12 14:26:46 -0400

    add apps to installed_apps

--- geovault/settings.py
+++ geovault/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core.apps.CoreConfig",
+    "core",
+    'accounts',
+    'geodata',
+    'leaflet',
+    'djgeojson',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -121,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
